ccf5fdc611c567f33a08ba963917f190c6a4f690
TusharPant <133578207+Technophile58@users.noreply.github.com>
2026-01-22 11:45:02 +0530

    Add files via upload

---
+++ RightAlignedInvertedTriangle.java
@@ -0,0 +1,14 @@
+public class RightAlignedInvertedTriangle {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int space = 0; space < n - i; space++)
+                System.out.print(" ");
+            for (int star = 1; star <= i; star++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
